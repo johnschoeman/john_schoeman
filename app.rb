d74024ddc851361d5acdceb3739abe6e6e6edfe4
John Schoeman <johnschoeman1617@gmail.com>
2018-01-05 11:03:05 -0500

    Create app and config file

--- app.rb
+++ app.rb
@@ -1,6 +1,56 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/reloader'
+require 'sprockets'
+require 'uglifier'
 
-get '/' do
-  erb :index
+class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  set :environment, Sprockets::Environment.new
+
+  environment.append_path "public/stylesheets"
+  environment.append_path "public/javascripts"
+
+  environment.js_compressor  = Uglifier.new(harmony: true)
+  environment.css_compressor = :scss
+
+  get "/assets/*" do
+    env["PATH_INFO"].sub!("/assets/", "")
+    settings.environment.call(env)
+  end
+
+
+  get '/projects' do
+    @projects = [
+      Project.new('Product Hunt App', 'productjadt.com', 'a cool site'),
+      Project.new('Ready Responder Alert', 'readyresponderalert.com', 'a cool iot project')
+    ]
+    erb :index, :layout => false do
+      erb :projects
+    end
+  end
+
+  get '/resume' do
+    erb :index, :layout => false do
+      erb :resume
+    end
+  end
+
+  get '*' do
+    erb :index, :layout => false do
+      erb :about
+    end
+  end
+end
+
+class Project
+  attr_accessor :title, :link_url, :description
+  
+  def initialize(title, link_url, description)
+    @title = title
+    @link_url = link_url
+    @description = description
+  end
 end
