9d39934bd88a484f756f7c9810aad955b62f6c25
John Schoeman <johnschoeman1617@gmail.com>
2018-01-08 13:51:39 -0500

    Update projects view and model

--- models/project.rb
+++ models/project.rb
@@ -1,11 +1,12 @@
 require 'csv'
 
 class Project
-  attr_accessor :title, :link_url, :date, :description
+  attr_accessor :title, :github_url, :site_url, :date, :description
   
-  def initialize(title, link_url, date, description)
+  def initialize(title, github_url, site_url, date, description)
     @title = title
-    @link_url = link_url
+    @github_url = github_url
+    @site_url = site_url
     @date = date
     @description = description
   end
@@ -14,6 +15,6 @@ class Project
     file = File.expand_path("../projects.csv", __FILE__)
     data = CSV.read(file, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all })
     hashed_data = data.map { |row| row.to_hash }
-    hashed_data.map { |row| new(row[:title], row[:link_url], row[:date], row[:description]) }
+    hashed_data.map { |row| new(row[:title], row[:github_url], row[:site_url], row[:date], row[:description]) }
   end
 end
