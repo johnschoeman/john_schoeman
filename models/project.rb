fb54e2e10b55ba6e8f42f896694a37d80fb7ce90
John Schoeman <johnschoeman1617@gmail.com>
2018-02-14 13:22:43 -0500

    Add skills and style to projects

--- models/project.rb
+++ models/project.rb
@@ -1,20 +1,21 @@
 require 'csv'
 
 class Project
-  attr_accessor :title, :github_url, :site_url, :date, :description
+  attr_accessor :title, :github_url, :site_url, :date, :description, :skills
   
-  def initialize(title, github_url, site_url, date, description)
+  def initialize(title, github_url, site_url, date, description, skills)
     @title = title
     @github_url = github_url
     @site_url = site_url
     @date = date
     @description = description
+    @skills = skills.split('--')
   end
 
   def self.all
     file = File.expand_path("../projects.csv", __FILE__)
     data = CSV.read(file, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all })
     hashed_data = data.map { |row| row.to_hash }
-    hashed_data.map { |row| new(row[:title], row[:github_url], row[:site_url], row[:date], row[:description]) }
+    hashed_data.map { |row| new(row[:title], row[:github_url], row[:site_url], row[:date], row[:description], row[:skills]) }
   end
 end
